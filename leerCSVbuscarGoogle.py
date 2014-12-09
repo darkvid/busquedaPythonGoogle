82d094495cc1ca090e81881caa5e98f8f449e026
darkvid <David Galisteo>
2014-12-09 10:33:55 +0100

    mejora del código y adición de comentarios

--- leerCSVbuscarGoogle.py
+++ leerCSVbuscarGoogle.py
@@ -2,28 +2,27 @@
 import csv
 from pygoogle import pygoogle
 
-reader = csv.reader(open('/Users/davidgalisteo/Desktop/scriptsPython/profesor_contrasena.csv', 'rb'))
-writer = open('/Users/davidgalisteo/Desktop/scriptsPython/contrasenas.txt', "w")
+reader = csv.reader(open('/Users/davidgalisteo/Desktop/scriptsPython/buscarGoogle/profesor_contrasena.csv', 'rb'))
+writer = open('/Users/davidgalisteo/Desktop/scriptsPython/buscarGoogle/resultado.txt', "w")
 
-hashes = [];
-
+#recorro todas las filas
 for row in reader:
-    try:
-        profesor = row[0]
-        password = row[1]
+    #obtengo el profesor y el password
+    profesor = row[0]
+    password = row[1]
 
-    except IndexError:
-        print 'index error'
-
+    #busco en google
     g = pygoogle(password)
+    #seteo número de páginas
     g.pages = 1
 
+    #si obtengo 1 o más resultados, los escribo en otro fichero
     if(g.get_result_count() > 0)
         print '*Hay %s resultados*'%(g.get_result_count())
         writer.write(profesor)
         writer.write('\n')
         writer.write(g.get_urls())
-        hashes.append(password)
         writer.write('\n')
 
+#cierro fichero
 writer.close()
